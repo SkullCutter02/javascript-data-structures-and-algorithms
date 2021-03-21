b2c9d7a20080572891f0f93e63e1868959b0156e
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-03-21 14:19:00 +0800

    BinarySearchTree

---
+++ 13. Binary Search Trees/BinarySearchTree.js
@@ -0,0 +1,76 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (value === current.value) return undefined;
+        else if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) return undefined;
+
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+
+    if (!found) return undefined;
+
+    return current;
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(5);
+tree.insert(5);
+tree.insert(7);
+tree.insert(1);
+tree.insert(10);
+tree.insert(9);
+tree.insert(0);
+tree.insert(-5);
+console.log(tree);
+console.log(tree.find(7));
